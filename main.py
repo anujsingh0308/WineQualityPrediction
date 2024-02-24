24562c6e4d206f9a26bdc6c782e32f19c71500ae
Anuj Singh <rajawatanuj890@gmail.com>
2024-02-24 16:30:37 +0530

    data ingestion added

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from Wine_Quality_Prediction import logger
+from Wine_Quality_Prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("This is a test info message")  
+
+STAGE_NAME = "Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
