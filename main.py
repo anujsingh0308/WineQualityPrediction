579dc4f87448c7ae65658ae796f922ce6f67de81
Anuj Singh <rajawatanuj890@gmail.com>
2024-03-04 23:29:48 +0530

    model trainer added

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from Wine_Quality_Prediction import logger
 from Wine_Quality_Prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Wine_Quality_Prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from Wine_Quality_Prediction.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
-
+from Wine_Quality_Prediction.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 
@@ -37,3 +37,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e 
+
+
+STAGE_NAME = "Model Training stage" 
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_training = ModelTrainerTrainingPipeline()
+    model_training.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:  
+    logger.exception(e)
+    raise e
